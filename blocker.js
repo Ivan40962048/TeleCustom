a01df7c5de18d5ba2ae0b45a2f18ce0ad5ec1070
Aravador <flash.vanya@gmail.com>
2024-04-08 14:43:11 +0500

    добавил изменение всех аватарок чатов на какую-то определенную

--- blocker.js
+++ blocker.js
@@ -2,8 +2,12 @@ function removeElements() {
 	chatIdsForRemove = ["957483527", "1741221590", "5652056591"];
 	let chats = document.querySelector("#folders-container > div > div.chatlist-top > ul").childNodes;
 	let searchedChat;
-	for (let k = 0; k < chatIdsForRemove.length; k++) {
-		for (let i = 0; i < chats.length; i++) {
+	for (let i = 0; i < chats.length; i++) {
+		let chatImages = chats[i].getElementsByTagName("img");
+		if (chatImages.length > 0)
+			chatImages[chatImages.length - 1].src = "https://i.pinimg.com/736x/a2/4d/d9/a24dd913965b66c8ff68d53cf899ce9e--game-creator-retro-gamer.jpg";
+
+		for (let k = 0; k < chatIdsForRemove.length; k++) {
 			if (chats[i].getAttribute("data-peer-id") == chatIdsForRemove[k])
 			{
 				searchedChat = chats[i];
