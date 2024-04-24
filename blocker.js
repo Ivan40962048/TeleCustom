c49b040ede2a3f28828e57eaf7cbe729fd7443e7
Aravador <flash.vanya@gmail.com>
2024-04-24 13:07:23 +0500

    Добавил возможность менять картинки аватарок у определенных чатов

--- blocker.js
+++ blocker.js
@@ -5,11 +5,11 @@ function run(){
 }
 
 function removeChatPreviews(chats) {
-	chatIdsForRemove = ["957483527", "1741221590", "5652056591", "504667562"];
+	chatIdsForRemovePreview = ["957483527", "1741221590", "5652056591", "504667562"];
 	let searchedChat;
 	for (let i = 0; i < chats.length; i++) {
-		for (let k = 0; k < chatIdsForRemove.length; k++) {
-			if (chats[i].getAttribute("data-peer-id") == chatIdsForRemove[k])
+		for (let k = 0; k < chatIdsForRemovePreview.length; k++) {
+			if (chats[i].getAttribute("data-peer-id") == chatIdsForRemovePreview[k])
 			{
 				searchedChat = chats[i];
 				searchedChat.querySelector("div.row-row.row-subtitle-row.dialog-subtitle > div.row-subtitle.no-wrap.dialog-subtitle-flex").remove();
@@ -19,10 +19,23 @@ function removeChatPreviews(chats) {
 }
 
 function changeChatAvatars(chats){
+	let imageReplacements = {
+		"957483527": "https://i.pinimg.com/564x/e0/9f/e8/e09fe8a8d4f25e12083117f8558df9f4.jpg",
+		"1741221590": "https://i.pinimg.com/736x/cf/59/33/cf5933428ec1997fa4c671e35a400d46.jpg",
+	};
 	for (let i = 0; i < chats.length; i++) {
+		let chatId = chats[i].getAttribute("data-peer-id");
 		let avatar = chats[i].getElementsByClassName("avatar")[0];
-		avatar.innerHTML = '<img class="avatar-photo" decoding="async" ' +
-		'src="https://i.pinimg.com/736x/a2/4d/d9/a24dd913965b66c8ff68d53cf899ce9e--game-creator-retro-gamer.jpg">';
+		if (Object.keys(imageReplacements).includes(chatId))
+		{
+			avatar.innerHTML = '<img class="avatar-photo" decoding="async" ' +
+			'src="' + imageReplacements[chatId] + '">';
+		}
+		else
+		{
+			avatar.innerHTML = '<img class="avatar-photo" decoding="async" ' +
+			'src="https://i.pinimg.com/736x/a2/4d/d9/a24dd913965b66c8ff68d53cf899ce9e--game-creator-retro-gamer.jpg">';
+		}
 	}
 }
 
